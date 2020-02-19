620c85219cd199c924f28b15e76025208c560986
wooou <dksdnwls@gmail.com>
2020-02-19 22:27:47 +0900

    Posts 등록기능 추가

---
+++ src/main/java/com/woo/study/springboot/service/PostsService.java
@@ -0,0 +1,19 @@
+package com.woo.study.springboot.service;
+
+import com.woo.study.springboot.domain.posts.PostsRepository;
+import com.woo.study.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
