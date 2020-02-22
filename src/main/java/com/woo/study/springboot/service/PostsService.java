5098afb92a7a5b1d32e431a47601e392b2f0721d
wooou <dksdnwls@gmail.com>
2020-02-22 12:39:47 +0900

    게시글 조회, 등록, 수정 화면 추가

--- src/main/java/com/woo/study/springboot/service/PostsService.java
+++ src/main/java/com/woo/study/springboot/service/PostsService.java
@@ -2,13 +2,16 @@ package com.woo.study.springboot.service;
 
 import com.woo.study.springboot.domain.posts.Posts;
 import com.woo.study.springboot.domain.posts.PostsRepository;
+import com.woo.study.springboot.web.dto.PostsListResponseDto;
 import com.woo.study.springboot.web.dto.PostsResponseDto;
 import com.woo.study.springboot.web.dto.PostsSaveRequestDto;
 import com.woo.study.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -37,4 +40,10 @@ public class PostsService {
         return new PostsResponseDto(entity);
     }
 
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
 }
