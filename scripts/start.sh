44a6e0e0f948eb21db0495803ba166e348ccec24
wooou <dksdnwls@gmail.com>
2020-04-15 23:37:19 +0900

    etc: script(start.sh) 오타 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -9,9 +9,9 @@ REPOSITORY=/home/ec2-user/app/step3
 PROJECT_NAME=springboot2-webservice
 
 echo "> Build 파일 복사"
-echo "> cp $REPOSITORY/zip/*.jar $REPOSITORY"
+echo "> cp $REPOSITORY/zip/*.jar $REPOSITORY/"
 
-cp $REPOSITORY/zip/*.jar $REPOSITORY
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 새 애플리케이션 배포"
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
@@ -29,5 +29,5 @@ IDLE_PROFILE=$(find_idle_profile)
 echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
 nohup java -jar \
   -Dspring.config.location=classpath:/application.yml,classpath:/application-$IDLE_PROFILE.yml,/home/ec2-user/app/application-oauth.yml,/home/ec2-user/app/application-real-db.yml \
-  -Dspring.profile.active=$IDLE_PROFILE \
+  -Dspring.profiles.active=$IDLE_PROFILE \
   $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
